dc34cbdc435c3b510f9b5cb332b3ef549874e138
amaan-mohib <amaan.mohib@hotmail.com>
2021-05-28 19:33:54 +0530

    auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import './App.css';
-import Landing from './Landing';
-import Dashboard from './Dashboard';
-import {user} from '../firebase';
+import React from "react";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import firebase from "firebase/app";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
+  const [user] = useAuthState(firebase.auth());
   return (
     <div className="App">
       <header className="App-header">
-        <section>
-          {user ? <Dashboard/> : <Landing/>}
-        </section>
+        <section>{user ? <Dashboard /> : <Landing />}</section>
       </header>
     </div>
   );
 }
-
 
 export default App;
