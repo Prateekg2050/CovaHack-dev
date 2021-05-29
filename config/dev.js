c9188092bd8878d56cd29101c6a12270ef554ff5
prateek gupta <prateekg2050@gmail.com>
2021-05-30 03:39:48 +0530

    final change

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-    apiKey: "AIzaSyAGQeQ2XJpnN3H9SU8Wk2MVNQNHAdDfBRc",
-    authDomain: "covahack.firebaseapp.com",
-    projectId: "covahack",
-    storageBucket: "covahack.appspot.com",
-    messagingSenderId: "28401822968",
-    appId: "1:28401822968:web:2ad351d605f0f1b11f9f0d",
-    measurementId: "G-WDYEK2Q9HE"
-}
--- config/keys.js
+++
@@ -1,8 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    // we are in production 
-    module.exports = require('./prod');
-}
-else{
-//we are in devlopment side
-module.exports = require('./dev');
-}
